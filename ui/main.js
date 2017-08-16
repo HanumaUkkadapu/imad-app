5c311d54544fbcadffb2a60cec16ef23ba5ab4c3
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 20:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,5 +28,21 @@ button.onclick = function (){
     //make a request to the counter end point
     request.open('GET' , 'http://utvhanuma03.imad.hasura-app.io/counter' , true);
     request.send(null);
-      
+};
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make a request to the server and send the name
+    
+    //capture a list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>' ;
+    }
+    var ul = document.getElementById('list');
+    ul.innerHTML = list;
 };
