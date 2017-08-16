94f15745b24e0084d1fddb3429970fcf3dd30c0c
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 18:08:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,14 @@
 console.log('Loaded!');
+
 // chaning the content with javascript
 //div element with id="first"
+
 var so = document.getElementById('first');
 so.innerHTML = "Just made some changes" ;
+
+//making the img move on click by adding an onclick event
+
+var pic = document.getElementById('madi');
+pic.onclick = function (){
+    pic.style.marginLeft = '200px';
+};
