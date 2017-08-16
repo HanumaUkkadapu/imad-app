645d45e255e025cee99ecd4e950c66a1309575db
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 20:04:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,30 +3,30 @@ var button = document.getElementById('counter');
 
 button.onclick = function (){
   
-  //create a request object
-  var request = new XMLHttpRequest();
-  
-  //we will write what to do with the response before makking a request
-  //capture the response and store it in a variable
-  request.onreadystatechange = function (){
+    //create a request object
+    var request = new XMLHttpRequest();
       
-      //checks the current status of request tha it tis successfully completed ( XMLHttpRequest.DONE )
-      if (requst.readyState === XMLHttpRequest.DONE ){
+    //we will write what to do with the response before makking a request
+    //capture the response and store it in a variable
+    request.onreadystatechange = function (){
           
-          //Take some action
-          //checks that the request is successful or not
-          if (request.status === 200 ){
-              var counter = request.responseText ;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-          
-      }
-      //Not done yet
-  };
-  
-  //make a request to the counter end point
-  request.open('GET' , 'http://utvhanuma03.imad.hasura-app.io/counter' , true);
-  request.send(null);
-  
+        //checks the current status of request tha it tis successfully completed ( XMLHttpRequest.DONE )
+        if (requst.readyState === XMLHttpRequest.DONE ){
+              
+            //Take some action
+            //checks that the request is successful or not
+            if (request.status === 200 ){
+                var counter = request.responseText ;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+              
+        }
+        //Not done yet
+    };
+      
+    //make a request to the counter end point
+    request.open('GET' , 'http://utvhanuma03.imad.hasura-app.io/counter' , true);
+    request.send(null);
+      
 };
