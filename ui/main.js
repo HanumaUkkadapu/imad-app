9c36af1c0959ba1d6e6c2b0f07f0a1070dcc92d8
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 19:45:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ btn.onclick = function (){
   
   //we will write what to do with the response before makking a request
   //capture the response and store it in a variable
-  request.onreadystatechange = function(){
+  request.onreadystatechange = function (){
       
       //checks the current status of request tha it tis successfully completed ( XMLHttpRequest.DONE )
       if (requst.readystate === XMLHttpRequest.DONE ){
