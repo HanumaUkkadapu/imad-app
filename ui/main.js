acedab6eaea048f3075561085d811a08639d822e
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 19:32:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,30 @@
 //counter code
 var btn = document.getElementById('counter');
-var counter = 0;
+
 btn.onclick = function (){
   
-  //make an request to counter end point
+  //create a request object
+  var request = new XMLHttpRequest();
   
   //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      
+      //checks the current status of request tha it tis successfully completed ( XMLHttpRequest.DONE )
+      if (requst.readystate === XMLHttpRequest.DONE ){
+          
+          //Take some action
+          //checks that the request is successful or not
+          if (request.status === 200 ){
+              var counter = request.responseText ;
+              counter = counter + 1;
+              span = document.getElementById('count');
+              span.innerHTML = counter;
+          }
+          
+      }
+      //Not done yet
+  };
   
-  //render the variable in the correct span
-  counter = counter + 1;
-  span = document.getElementById('count');
-  span.innerHTML = counter;
+  //make a request to the counter end point
+  
 };
