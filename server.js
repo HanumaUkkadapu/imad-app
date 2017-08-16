8fa38bec0f219bb5de5ab3850108e6d27a10c912
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 20:54:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,9 +108,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name/:name', function(req, res){//URL : /submit-name?name=XXXXXX
     //get the name from the request object
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     
     // JSON: JavScript Object Notation - to converts JavaScript objects to string
