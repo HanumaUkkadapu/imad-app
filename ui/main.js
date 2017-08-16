a5b760d21f3bd88779d7c03dd7a478903d839c48
HanumaUkkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2017-08-16 19:38:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ btn.onclick = function (){
   //create a request object
   var request = new XMLHttpRequest();
   
+  //we will write what to do with the response before makking a request
   //capture the response and store it in a variable
   request.onreadystatechange = function(){
       
@@ -26,5 +27,7 @@ btn.onclick = function (){
   };
   
   //make a request to the counter end point
+  request.open('GET' , 'http://utvhanuma03.imad.hasura-app.io/counter' , true);
+  request.send(null);
   
 };
